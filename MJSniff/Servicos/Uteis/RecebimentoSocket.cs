d3fe3409247eb563045234d101bb41c2e6d84f03
Unknown <ramonss.bque@gmail.com>
2018-05-23 17:57:18 -0300

    checkin

---
+++ MJSniff/Servicos/Uteis/RecebimentoSocket.cs
@@ -0,0 +1,140 @@
+﻿using MJsniffer;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MJSniff.Servicos
+{
+    public class RecebimentoSocket
+    {
+        public static byte[] ByteData = new byte[4096];
+        public static Socket MainSocket { get; set; } //The socket which captures all incoming packets
+        public static bool bContinueCapturing = false;            //A flag to check if packets are to be captured or not
+        public static void OnReceive(IAsyncResult ar)
+        {
+            try
+            {
+                int nReceived = MainSocket.EndReceive(ar);
+
+                //Analyze the bytes received...
+
+                ParseData(ByteData, nReceived);
+
+                if (bContinueCapturing)
+                {
+                    ByteData = new byte[4096];
+
+                    //Another call to BeginReceive so that we continue to receive the incoming
+                    //packets
+                    MainSocket.BeginReceive(ByteData, 0, ByteData.Length, SocketFlags.None,
+                        new AsyncCallback(OnReceive), null);
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "MJsniffer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+       
+        //Helper function which returns the information contained in the IP header as a
+        //tree node
+        public static TreeNode MakeIPTreeNode(IPHeader ipHeader)
+        {
+            TreeNode ipNode = new TreeNode();
+
+            ipNode.Text = "IP";
+            ipNode.Nodes.Add("Ver: " + ipHeader.Version);
+            ipNode.Nodes.Add("Header Length: " + ipHeader.HeaderLength);
+            ipNode.Nodes.Add("Differntiated Services: " + ipHeader.DifferentiatedServices);
+            ipNode.Nodes.Add("Total Length: " + ipHeader.TotalLength);
+            ipNode.Nodes.Add("Identification: " + ipHeader.Identification);
+            ipNode.Nodes.Add("Flags: " + ipHeader.Flags);
+            ipNode.Nodes.Add("Fragmentation Offset: " + ipHeader.FragmentationOffset);
+            ipNode.Nodes.Add("Time to live: " + ipHeader.TTL);
+            switch (ipHeader.ProtocolType)
+            {
+                case Protocol.TCP:
+                    ipNode.Nodes.Add("Protocol: " + "TCP");
+                    break;
+                case Protocol.UDP:
+                    ipNode.Nodes.Add("Protocol: " + "UDP");
+                    break;
+                case Protocol.Unknown:
+                    ipNode.Nodes.Add("Protocol: " + "Unknown");
+                    break;
+            }
+            ipNode.Nodes.Add("Checksum: " + ipHeader.Checksum);
+            ipNode.Nodes.Add("Source: " + ipHeader.SourceAddress.ToString());
+            ipNode.Nodes.Add("Destination: " + ipHeader.DestinationAddress.ToString());
+
+            return ipNode;
+        }
+
+        //Helper function which returns the information contained in the TCP header as a
+        //tree node
+        public static TreeNode MakeTCPTreeNode(TCPHeader tcpHeader)
+        {
+            TreeNode tcpNode = new TreeNode();
+
+            tcpNode.Text = "TCP";
+
+            tcpNode.Nodes.Add("Source Port: " + tcpHeader.SourcePort);
+            tcpNode.Nodes.Add("Destination Port: " + tcpHeader.DestinationPort);
+            tcpNode.Nodes.Add("Sequence Number: " + tcpHeader.SequenceNumber);
+
+            if (tcpHeader.AcknowledgementNumber != "")
+                tcpNode.Nodes.Add("Acknowledgement Number: " + tcpHeader.AcknowledgementNumber);
+
+            tcpNode.Nodes.Add("Header Length: " + tcpHeader.HeaderLength);
+            tcpNode.Nodes.Add("Flags: " + tcpHeader.Flags);
+            tcpNode.Nodes.Add("Window Size: " + tcpHeader.WindowSize);
+            tcpNode.Nodes.Add("Checksum: " + tcpHeader.Checksum);
+
+            if (tcpHeader.UrgentPointer != "")
+                tcpNode.Nodes.Add("Urgent Pointer: " + tcpHeader.UrgentPointer);
+
+            return tcpNode;
+        }
+
+        //Helper function which returns the information contained in the UDP header as a
+        //tree node
+        public static TreeNode MakeUDPTreeNode(UDPHeader udpHeader)
+        {
+            TreeNode udpNode = new TreeNode();
+
+            udpNode.Text = "UDP";
+            udpNode.Nodes.Add("Source Port: " + udpHeader.SourcePort);
+            udpNode.Nodes.Add("Destination Port: " + udpHeader.DestinationPort);
+            udpNode.Nodes.Add("Length: " + udpHeader.Length);
+            udpNode.Nodes.Add("Checksum: " + udpHeader.Checksum);
+
+            return udpNode;
+        }
+
+        //Helper function which returns the information contained in the DNS header as a
+        //tree node
+        public static TreeNode MakeDNSTreeNode(byte[] byteData, int nLength)
+        {
+            DNSHeader dnsHeader = new DNSHeader(byteData, nLength);
+
+            TreeNode dnsNode = new TreeNode();
+
+            dnsNode.Text = "DNS";
+            dnsNode.Nodes.Add("Identification: " + dnsHeader.Identification);
+            dnsNode.Nodes.Add("Flags: " + dnsHeader.Flags);
+            dnsNode.Nodes.Add("Questions: " + dnsHeader.TotalQuestions);
+            dnsNode.Nodes.Add("Answer RRs: " + dnsHeader.TotalAnswerRRs);
+            dnsNode.Nodes.Add("Authority RRs: " + dnsHeader.TotalAuthorityRRs);
+            dnsNode.Nodes.Add("Additional RRs: " + dnsHeader.TotalAdditionalRRs);
+
+            return dnsNode;
+        }
+
+    }
+}
